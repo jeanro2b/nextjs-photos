3e876698b2562042c951869a19e7def272b0b538
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-01-21 12:54:22 +0100

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -10,7 +10,7 @@ async function handler(req, res) {
 
     const { title, image, address, description } = data; // pas forcément besoin 
 
-    const client = await MongoClient.connect('mongodb+srv://jeanro2b:cmdrate2012@cluster0.xcf5fdg.mongodb.net/?retryWrites=true&w=majority')
+    const client = await MongoClient.connect('mongodb+srv://jeanro2b:password@cluster0.xcf5fdg.mongodb.net/?retryWrites=true&w=majority')
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups'); // on peut donner le nom que l'on veut
